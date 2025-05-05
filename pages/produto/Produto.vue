1daa2eca85259f4604edd24ae9aa14524352c708
Davi <davi.edfisica@outlook.com>
2025-05-05 22:12:32 +0100

    Atualização da rota de produtos: captura de ID e exibição dinâmica

---
+++ pages/produto/Produto.vue
@@ -0,0 +1,3 @@
+<template><div>produto<div>{{ produto.nome }}</div></div></template>
+<script setup>import { useRoute } from "vue-router";const route = useRoute();const produtos = [{ id: 1, nome: "Camiseta", preco: 25 },{ id: 2, nome: "Tênis", preco: 100 },{ id: 3, nome: "Relógio", preco: 200 }];const produto = produtos.find((p) => p.id == route.params.id);</script>
+
